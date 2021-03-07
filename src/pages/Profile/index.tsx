1a6a6c499621bb7be2c8e7443dcf228a44e2afed
Milena <milenahikari11@gmail.com>
2021-03-07 13:42:24 -0300

    Ajuste oneOf

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -54,7 +54,7 @@ const Profile: React.FC = () => {
               then: Yup.string().required('Campo obrigatório'),
               otherwise: Yup.string(),
             })
-            .oneOf([Yup.ref('password'), null, 'Confirmação inválida']),
+            .oneOf([Yup.ref('password'), null], 'Confirmação inválida'),
         });
 
         await schema.validate(data, {
